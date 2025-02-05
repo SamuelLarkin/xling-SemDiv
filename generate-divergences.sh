ae5530fa9216d4a6773d4c7530f138a459500acf
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2025-02-05 07:59:22 -0500

    feat: added generate divergences scripts

--- generate-divergences.sh
+++ generate-divergences.sh
@@ -18,11 +18,12 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/fs/clip-scratch/ebriakou/anaconda3/lib
 
 # ==== Set directories
-root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-data_dir=$root_dir/data
-scripts_dir=$root_dir/source
-seed_file=WikiMatrix.$1-$2.tsv.filtered_sample_50000.moses.seed
-seeds=$data_dir/wikimatrix/$seed_file
+readonly root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly data_dir=$root_dir/data
+readonly scripts_dir=$root_dir/source
+readonly seed_file=WikiMatrix.$1-$2.tsv.filtered_sample_50000.moses.seed
+readonly seeds=$data_dir/wikimatrix/$seed_file
+readonly job_id=${SLURM_JOBID:+-$SLURM_JOBID}
 
 
 # === Set dependencies
@@ -31,40 +32,38 @@ python -m spacy download 'en_core_web_sm'
 
 echo $'\n> Generate synthetic divergences from seed equivalents'
 for process in i u d r g p; do 
+  echo $'\n--- Divergent type: '$process$' ---\n'
 
-		echo $'\n--- Divergent type: '$process$' ---\n'
+  time python "$scripts_dir/generate_divergent_data.py" \
+    --mode $process \
+    --data "$seeds" \
+    --output synthetic$job_id \
+    --bert_local_cache pretrained_bert \
+    --pretrained_bert "bert-base-cased" \
+    #--debug
+done
 
-		python $scripts_dir/generate_divergent_data.py \
-						--mode $process \
-						--data $seeds \
-						--output synthetic \
-						--bert_local_cache pretrained_bert \
-						--pretrained_bert "bert-base-cased" \
-						#--debug
-
-done		
-
-cut -f 1-2 $seeds > ${seeds}_exclude_align
+cut -f 1-2 "$seeds" > "${seeds}_exclude_align"
 
 echo $'\n> Prepare divergence ranking for sentence-level divergentmBERT'
-python $scripts_dir/build_bert_training_data.py \
-                    --path_to_unlabeled ${seeds}_exclude_align \
-                    --path_to_divergences $root_dir/synthetic/from_$seed_file \
-                    --divergent_list rdpg \
-                    --contrastive \
-                    --learn-to-rank \
-                    --divergence-ranking
+time python "$scripts_dir/build_bert_training_data.py" \
+  --path_to_unlabeled "${seeds}_exclude_align" \
+  --path_to_divergences "$root_dir/synthetic$job_id/from_$seed_file" \
+  --divergent_list rdpg \
+  --contrastive \
+  --learn-to-rank \
+  --divergence-ranking
 
 echo $'\n> Prepare divergence ranking for multi-task divergentmBERT'
-python $scripts_dir/build_bert_training_data.py \
-                    --path_to_unlabeled ${seeds}_exclude_align \
-                    --path_to_divergences $root_dir/synthetic/from_$seed_file \
-                    --divergent_list rdpg \
-                    --contrastive \
-                    --learn-to-rank \
-                    --divergence-ranking \
-                    --multi-task
+time python "$scripts_dir/build_bert_training_data.py" \
+  --path_to_unlabeled "${seeds}_exclude_align" \
+  --path_to_divergences "$root_dir/synthetic$job_id/from_$seed_file" \
+  --divergent_list rdpg \
+  --contrastive \
+  --learn-to-rank \
+  --divergence-ranking \
+  --multi-task
 
-label_file=$root_dir/for_divergentmBERT/from_$seed_file/contrastive_multi_hard/rdpg/labels.txt
-echo "O" > $label_file
-echo "D" >> $label_file
+readonly label_file=$root_dir/for_divergentmBERT/from_$seed_file/contrastive_multi_hard/rdpg/labels.txt
+echo "O" > "$label_file"
+echo "D" >> "$label_file"

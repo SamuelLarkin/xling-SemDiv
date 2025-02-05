b8eab0382700d3082c9ae8befb400516bf387307
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2025-02-05 08:49:52 -0500

    feat: blacked and linted

--- predict-token-tags.sh
+++ predict-token-tags.sh
@@ -19,41 +19,41 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/fs/clip-scratch/ebriakou/anaconda3/lib
 
-corpus=WikiMatrix                           #   Corpus from which seed equivalents are extracted
-sampling_method=contrastive_multi_hard      #   Sampling method for extracting divergent examples from seeds
-size=50000                                  #   Number of seeds sampled from original corpus
-src=en                                      #   Source language (language code)
-tgt=fr                                      #   Target language (language code)
-divergent_list=rdpg                         #   List of divergences (e.g, 'rd' if divergences include
+readonly corpus=WikiMatrix                           #   Corpus from which seed equivalents are extracted
+readonly sampling_method=contrastive_multi_hard      #   Sampling method for extracting divergent examples from seeds
+readonly size=50000                                  #   Number of seeds sampled from original corpus
+readonly src=en                                      #   Source language (language code)
+readonly tgt=fr                                      #   Target language (language code)
+readonly divergent_list=rdpg                         #   List of divergences (e.g, 'rd' if divergences include
                                             #                  phrase replacement and subtree deletion)
 
 #################################################################################
 
-root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-data_dir=$root_dir/data
-scripts_dir=$root_dir/source
+readonly root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly data_dir=$root_dir/data
+readonly scripts_dir=$root_dir/source
 
-exp_identifier=from_${corpus}.${src}-${tgt}.tsv.filtered_sample_${size}.moses.seed/${sampling_method}/${divergent_list}
-data_dir=$root_dir/for_divergentmBERT/${exp_identifier}
-output_dir=$root_dir/trained_bert/$exp_identifier
-REFreSD_dir=$root_dir/REFreSD/REFreSD_for_huggingface
-model=bert-base-multilingual-cased
+readonly exp_identifier=from_${corpus}.${src}-${tgt}.tsv.filtered_sample_${size}.moses.seed/${sampling_method}/${divergent_list}
+readonly data_dir=$root_dir/for_divergentmBERT/${exp_identifier}
+readonly output_dir=$root_dir/trained_bert/$exp_identifier
+readonly REFreSD_dir=$root_dir/REFreSD/REFreSD_for_huggingface
+readonly model=bert-base-multilingual-cased
 
 ###################################################################################
 #          Token level predictions on REFreSD	                                  #
 ###################################################################################
 
-set_=test
+readonly test_set="test"
 
-python $scripts_dir/run_div_multi.py \
-        	--node $SLURM_NODELIST \
-        	--model_type SemDivMulti \
-        	--model_name_or_path $model \
-        	--task_name SemDiv \
-        	--do_eval   \
-        	--best_checkpoint \
-        	--evaluation_set $set_ \
-        	--data_dir $data_dir/   \
-        	--output_dir $output_dir \
-        	--synth_data_dir $REFreSD_dir/ \
-        	--overwrite_cache
+python "$scripts_dir/run_div_multi.py" \
+  --node "$SLURM_NODELIST" \
+  --model_type SemDivMulti \
+  --model_name_or_path $model \
+  --task_name SemDiv \
+  --do_eval   \
+  --best_checkpoint \
+  --evaluation_set $test_set \
+  --data_dir "$data_dir/"   \
+  --output_dir "$output_dir" \
+  --synth_data_dir "$REFreSD_dir/" \
+  --overwrite_cache
